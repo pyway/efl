7170232584253cde40b7ac583e5c0a8195597b64
Daniel Hirt <hirt.danny@gmail.com>
2017-12-13 23:51:07 +0200

    add markup mixin WIP

---
+++ src/lib/efl/interfaces/efl_text_markup_mixin.c
@@ -0,0 +1,180 @@
+#include "config.h"
+#include "Efl.h"
+
+#define MY_CLASS EFL_TEXT_MARKUP_MIXIN_CLASS
+
+#define ERR(...) EINA_LOG_DOM_ERR(EINA_LOG_DOMAIN_DEFAULT, __VA_ARGS__)
+
+#define _REPLACEMENT_CHAR 0xFFFC
+#define _PARAGRAPH_SEPARATOR 0x2029
+#define _NEWLINE '\n'
+#define _TAB '\t'
+
+#define _REPLACEMENT_CHAR_UTF8 "\xEF\xBF\xBC"
+#define _PARAGRAPH_SEPARATOR_UTF8 "\xE2\x80\xA9"
+#define _NEWLINE_UTF8 "\n"
+#define _TAB_UTF8 "\t"
+#define EVAS_TEXTBLOCK_IS_VISIBLE_FORMAT_CHAR(ch) \
+   (((ch) == _REPLACEMENT_CHAR) || \
+    ((ch) ==  _NEWLINE) || \
+    ((ch) == _TAB) || \
+    ((ch) == _PARAGRAPH_SEPARATOR))
+
+#define _IS_TAB(item)                                             \
+   (!strcmp(item, "tab") || !strcmp(item, "\t") || !strcmp(item, "\\t"))
+
+#define _IS_LINE_SEPARATOR(item)                                             \
+   (!strcmp(item, "br") || !strcmp(item, "\n") || !strcmp(item, "\\n"))
+
+#define _IS_PARAGRAPH_SEPARATOR_SIMPLE(item)                                 \
+   (!strcmp(item, "ps"))
+
+#define _IS_PARAGRAPH_SEPARATOR(o, item)                                     \
+   (_IS_PARAGRAPH_SEPARATOR_SIMPLE(item) ||                                  \
+    (o->legacy_newline && _IS_LINE_SEPARATOR(item))) /* Paragraph separator */
+
+typedef struct _Efl_Text_Markup_Mixin_Data Efl_Text_Markup_Mixin_Data;
+
+struct _Efl_Text_Markup_Mixin_Data
+{
+   char *markup;
+};
+
+static void
+_prepend_text_run(Eo *eo_obj, Efl_Text_Cursor_Cursor *cur,
+      const char *s, const char *p)
+{
+   if ((s) && (p > s))
+     {
+        char *ts;
+
+        ts = alloca(p - s + 1);
+        strncpy(ts, s, p - s);
+        ts[p - s] = 0;
+        //evas_textblock_cursor_text_prepend(cur, ts);
+        efl_text_format_insert(eo_obj, cur, ts);
+     }
+}
+
+static EOLIAN void
+_efl_text_markup_mixin_markup_insert(Eo *eo_obj, Efl_Text_Markup_Mixin_Data *o,
+      Efl_Text_Cursor_Cursor *cur, const char *text)
+{
+   if (text)
+     {
+        char *s, *p;
+        char *tag_start, *tag_end, *esc_start, *esc_end;
+
+        tag_start = tag_end = esc_start = esc_end = NULL;
+        p = (char *)text;
+        s = p;
+        /* This loop goes through all of the mark up text until it finds format
+         * tags, escape sequences or the terminating NULL. When it finds either
+         * of those, it appends the text found up until that point to the textblock
+         * proccesses whatever found. It repeats itself until the terminating
+         * NULL is reached. */
+        for (;;)
+          {
+             size_t text_len;
+             /* If we got to the end of string or just finished/started tag
+              * or escape sequence handling. */
+             if ((*p == 0) ||
+                   (tag_end) || (esc_end) ||
+                   (tag_start) || (esc_start))
+               {
+                  if (tag_end)
+                    {
+                       /* If we reached to a tag ending, analyze the tag */
+                       char *ttag;
+                       size_t ttag_len = tag_end - tag_start;
+
+
+                       ttag = malloc(ttag_len + 1);
+                       if (ttag)
+                         {
+                            memcpy(ttag, tag_start, ttag_len);
+                            ttag[ttag_len] = 0;
+                            //evas_textblock_cursor_format_prepend(cur, ttag);
+                            efl_text_format_insert(eo_obj, cur, ttag);
+                            free(ttag);
+                         }
+                       tag_start = tag_end = NULL;
+                    }
+                  else if (esc_end)
+                    {
+                       //_prepend_escaped_char(cur, esc_start, esc_end + 1);
+                       esc_start = esc_end = NULL;
+                    }
+                  else if (*p == 0)
+                    {
+                       _prepend_text_run(eo_obj, cur, s, p);
+                       s = NULL;
+                    }
+                  if (*p == 0)
+                    break;
+               }
+             if (*p == '<')
+               {
+                  if (!esc_start)
+                    {
+                       /* Append the text prior to this to the textblock and mark
+                        * the start of the tag */
+                       tag_start = p;
+                       tag_end = NULL;
+                       _prepend_text_run(eo_obj, cur, s, p);
+                       s = NULL;
+                    }
+               }
+             else if (*p == '>')
+               {
+                  if (tag_start)
+                    {
+                       tag_end = p + 1;
+                       s = p + 1;
+                    }
+               }
+             else if (*p == '&')
+               {
+                  if (!tag_start)
+                    {
+                       /* Append the text prior to this to the textblock and mark
+                        * the start of the escape sequence */
+                       esc_start = p;
+                       esc_end = NULL;
+                       _prepend_text_run(eo_obj, cur, s, p);
+                       s = NULL;
+                    }
+               }
+             else if (*p == ';')
+               {
+                  if (esc_start)
+                    {
+                       esc_end = p;
+                       s = p + 1;
+                    }
+               }
+             /* Unicode object replacement char */
+             else if (!strncmp(_REPLACEMENT_CHAR_UTF8, p,
+                      text_len = strlen(_REPLACEMENT_CHAR_UTF8)) ||
+                   !strncmp(_NEWLINE_UTF8, p,
+                      text_len = strlen(_NEWLINE_UTF8)) ||
+                   !strncmp(_TAB_UTF8, p,
+                      text_len = strlen(_TAB_UTF8)) ||
+                   !strncmp(_PARAGRAPH_SEPARATOR_UTF8, p,
+                      text_len = strlen(_PARAGRAPH_SEPARATOR_UTF8)))
+               {
+                  /*FIXME: currently just remove them, maybe do something
+                   * fancier in the future, atm it breaks if this char
+                   * is inside <> */
+                  _prepend_text_run(eo_obj, cur, s, p);
+                  /* it's also advanced later in this loop need +text_len
+                     in total*/
+                  p += text_len - 1;
+                  s = p + 1; /* One after the end of the replacement char */
+               }
+             p++;
+          }
+     }
+}
+
+#include "interfaces/efl_text_markup_mixin.eo.c"
