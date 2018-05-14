4a4ab7ef9874629d87913fa2fe93de202f736ea5
Marcel Hollerbach <marcel@osg.samsung.com>
2018-05-14 13:25:41 +0200

    bla

--- .ci/build-efl-app.sh
+++ .ci/build-efl-app.sh
@@ -9,6 +9,11 @@ git clone http://git.enlightenment.org/tools/examples.git/
 
 cd examples/apps/c/life/
 
+#get meson
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py --user
+pip install meson
+
 #build the example
 mkdir build
 meson . ./build
